3f17c892ca1485fc4339ceeb8e54944c2e515a78
Olaolu AKinsete <akinsbo@gmail.com>
2018-04-05 14:06:08 +0100

    added http component

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * MaryBoyeShow
+ *
+ * 2017 (c) Olaolu Akinsete <oa01@ledebe.com>
+ *
+ * This is video service for MaryBoyeShow.
+ *
+ * @author Olaolu Akinsete
+ */
+
+require __DIR__ . '/web/index.php';
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types = 1);
-
+require_once __DIR__ . '/../vendor/autoload.php';
 /*
  * MaryBoyeShow
  *
@@ -12,8 +12,14 @@ declare(strict_types = 1);
  * @author Olaolu Akinsete
  */
 
-$input = $_GET['name'] ?? 'World';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+$request = Request::createFromGlobals();
 
+$name = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
 
